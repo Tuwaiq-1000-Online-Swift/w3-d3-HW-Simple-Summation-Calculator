5e55d01bbf9ad8969828ccb6fd45732482a214aa
Yarob Al Mostafa (يعرُب) <yarob.hm@gmail.com>
2021-11-20 09:40:41 +0300

    latests changes

--- CalcHW/ViewController.swift
+++ CalcHW/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstNumberTextField: UITextField!
+    
+    @IBOutlet weak var secondNumberTextField: UITextField!
+    
+    
+    @IBOutlet weak var resutlLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calcButtonClicked(_ sender: Any) {
+        var number1 = firstNumberTextField.text!
+        var number2 = secondNumberTextField.text!
+        
+        var total = Int(number1)! + Int(number2)!
+        
+        resutlLabel.text = String(total)
+        
+    }
+    
 }
 
